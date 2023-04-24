5dd9ccf5fedc7674f5e30a338d38ef2afbc19fe3
JustRoberto <92164999+JustRoberto@users.noreply.github.com>
2023-04-25 00:50:41 +0300

    Add files via upload

---
+++ Journal/Journal/Controllers/Context.cs
@@ -0,0 +1,15 @@
+﻿using IO.Swagger.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Journal.Controllers
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Volume> Volumes { get; set; }
+    }
+}
